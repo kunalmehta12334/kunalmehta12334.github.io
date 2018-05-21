6d5fe54b8961d38a8f75cd2121a3a38941229a6b
kunalmehta12334 <kunalmehta2804@gmail.com>
2018-05-21 14:24:01 +0530

    incoming fles

---
+++ js/new_js.js
@@ -0,0 +1,22 @@
+$("label").slideUp(5);
+
+ $(".form1").keypress(function(){
+  	$(".label1").slideDown(500);
+  	$(".form1").removeAttr("placeholder");
+  });
+
+ $(".form2").keypress(function(){
+  	$(".label2").slideDown(500);
+  	$(".form2").removeAttr("placeholder");
+  });
+
+ $(".form3").keypress(function(){
+  	$(".label3").slideDown(500);
+  	$(".form3").removeAttr("placeholder");
+  });
+
+$(".form4").keypress(function(){
+ 	$(".label4").slideDown(500);
+ 	$(".form4").removeAttr("placeholder");
+ });
+
